a5b65675cd9035c0281a975f32fdf9c9438a52fa
oliver_zhang <568975137@qq.com>
2019-06-11 20:17:06 +0800

    重新提交特征点检测工程

---
+++ SearchFeaturePoints/SearchFeaturePoints.cpp
@@ -0,0 +1,13 @@
+// SearchFeaturePoints.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include<opencv2\opencv.hpp>
+#include<iostream>
+using namespace cv;
+
+int main()
+{
+
+    return 0;
+}
--- test1/test1.cpp
+++ test1/test1.cpp
@@ -2,12 +2,38 @@
 //
 
 #include "stdafx.h"
+#include<vector>
+#include <opencv2/opencv.hpp>
+using namespace std;
+using namespace cv;
 
 int main()
 {
-	int x = 0;
-	x = 1;
-	x = 2;
+	Mat img1 = imread("F:\\src1.png");
+	Mat img2 = imread("F:\\src2.png");
+
+	// 1. 初始化
+	vector<KeyPoint> keypoints1, keypoints2;
+	Mat descriptors1, descriptors2;
+	Ptr<ORB> orb = ORB::create();
+
+	// 2. 提取特征点
+	orb->detect(img1, keypoints1);
+	orb->detect(img2, keypoints2);
+
+	// 3. 计算特征描述符
+	orb->compute(img1, keypoints1, descriptors1);
+	orb->compute(img2, keypoints2, descriptors2);
+
+	// 4. 对两幅图像的BRIEF描述符进行匹配，使用BFMatch，Hamming距离作为参考
+	vector<DMatch> matches;
+	BFMatcher bfMatcher(NORM_HAMMING);
+	bfMatcher.match(descriptors1, descriptors2, matches);
+	for (const auto& it : matches)
+	{
+		cout << "it.imgIdx : " << it.imgIdx << endl;
+	}
+
     return 0;
 }
 
